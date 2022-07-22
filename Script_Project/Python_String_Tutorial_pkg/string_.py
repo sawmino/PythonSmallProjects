193a06c328e8230583c400fee0febacc1756160b
Saw Oo <elevatingstudio@gmail.com>
2022-07-22 15:08:06 +0800

    [UPD] test update

--- Script_Project/Python_String_Tutorial_pkg/string_.py
+++ Script_Project/Python_String_Tutorial_pkg/string_.py
@@ -11,10 +11,19 @@ def print_data():
     print('f"Hello, {name}" is '+ string_data_1)
     print('f"1+1 is {1+1}" is ' + string_data_2)
     print("-------------------------------------")
+    print("USING STACK & QUEUE LOGICALLY WITH SIMPLE LIST")
     print("using stack")
     stack=[]
     stack.append(1)
     stack.append(2)
-    print("using pop on current list                                                                       ")
-    pop_elem=stack.pop()
+    print("using pop on current stack list                                                                       ")
+    pop_elem=stack.pop() #the item will be removed from the last index.
     print(stack,pop_elem)
+    print("using queue")
+    queue=[]
+    queue.append(1)
+    queue.append(2)
+    print("using pop on current queue list                                                                       ")
+    pop_elem=queue.pop(0) # by using 0, item will be removed from the first index.
+    print(queue,pop_elem)
+    print("-------------------------------------")
